568fa3f5190571fb5d1f939342a38ec18589c807
Jordan Stilley <jordanstilley@gmail.com>
2020-05-15 08:47:04 +0200

    Initial code. just prints the initial board state

--- SudokuSolver/SudokuSolver.cpp
+++ SudokuSolver/SudokuSolver.cpp
@@ -1,13 +1,5 @@
 // SudokuSolver.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
@@ -18,3 +10,57 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+#include <iostream>
+
+int board[9][9];
+
+// int all_values[9]
+
+
+void get_initial_values() {
+    // TODO: Read these in from a file (or image??)
+    
+    //    r  c    From: https://en.wikipedia.org/wiki/Sudoku 
+    board[0][0] = 5;
+    board[0][1] = 3;
+    board[0][4] = 7;
+    //board[][] = ;
+    //board[][] = ;
+    //board[][] = ;
+    //board[][] = ;
+    //board[][] = ;
+    //board[][] = ;
+    //board[][] = ;
+    //board[][] = ;
+    //board[][] = ;
+    //board[][] = ;
+    //board[][] = ;
+    //board[][] = ;
+    //board[][] = ;
+    //board[][] = ;
+    //board[][] = ;
+    //board[][] = ;
+
+
+}
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    get_initial_values();
+
+    for (int row=0; row < 9; row++) {
+
+        for (int col = 0; col < 9; col++) {
+            printf("%d ", board[row][col]);
+        }
+
+        printf("\n");
+    }
+
+}
+
